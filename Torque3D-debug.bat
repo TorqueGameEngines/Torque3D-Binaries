c26e822add427af2bed46a474bf68ea4ec9d603c
Areloch <Areloch@gmail.com>
2022-12-20 13:44:28 -0600

    Binaries build for Torque3D v4.0.2

---
+++ Torque3D-debug.bat
@@ -0,0 +1,15 @@
+:: little debug helper script that helps you starting Torque3D with command line arguments.
+
+:: possible args:
+:: -log <Mode: 0,1,2>
+:: -console
+:: -level <level.mis>
+:: -worldeditor
+:: -guieditor
+:: -help
+
+:: as example, we just show the console
+"Torque3D.exe" -console
+
+:: or load a level and open the editor:
+:: "Torque3D.exe" -console -level mylevel.mis -worldeditor
